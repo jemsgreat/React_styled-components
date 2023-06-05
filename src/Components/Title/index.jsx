26487d1afd0c4d8549019b33de245efe04376b9e
Jhonattan Mercado <jmercadosenior@gmail.com>
2023-06-05 11:58:58 -0500

    Aula 2 finalizada

--- src/Components/Title/index.jsx
+++ src/Components/Title/index.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+/* import React from "react"; */
 import styled from "styled-components";
 
 const Title = styled.h1`
   color: grey;
   padding: 25px 0;
-`
+`;
 
 /* const Title = ({ children }) => {
   return <h1 className="title">{children}</h1>;
