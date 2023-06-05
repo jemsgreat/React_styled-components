b39f74e0b19c596cc05a474c87206150b81999a2
Jhonattan Mercado <jmercadosenior@gmail.com>
2023-06-05 13:10:03 -0500

    Aula 3 finalizada

--- src/Components/Account/index.jsx
+++ src/Components/Account/index.jsx
@@ -3,6 +3,12 @@ import React, { useState } from "react";
 import privado from "../../assets/images/privado.svg";
 import ojo from "../../assets/images/ojo.svg";
 import dinero from "../../assets/images/dinero.svg";
+import { Box, Btn, Detalle, Icono, Saldo } from "../UI";
+import styled from "styled-components";
+
+const IconoMargin = styled(Icono)`  /* styled(Icono) lo que hace es heredar los estilos del componente icono */
+  margin-Top: 2px;
+`
 
 const Account = () => {
   const [toggleState, untoggle] = useState(true);
@@ -11,30 +17,29 @@ const Account = () => {
     untoggle((toggleState) => !toggleState);
   };
 
+
   return (
-    <div className="box">
+    <Box>
       <h2>Cuenta</h2>
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponible
         <span>
-          <img className="imagen-icono" src={dinero} alt="Icono de saldo" />
+          <Icono src={dinero} alt="Icono de saldo" />
         </span>
         {toggleState ? (
-          <div className="saldo">
-            <span className="detalle">$</span> 8,621.50
-          </div>
+          <Saldo>
+            <Detalle>$</Detalle> 8,621.50
+          </Saldo>
         ) : null}
       </div>
 
-      <button className="btn" onClick={toggleHandler}>
-        <img
-          style={{ marginTop: "2px" }}
-          className="imagen-icono"
+      <Btn onClick={toggleHandler}>
+        <IconoMargin
           src={toggleState ? privado : ojo}
           alt="Privacidad de saldo"
         />
-      </button>
-    </div>
+      </Btn>
+    </Box>
   );
 };
 
